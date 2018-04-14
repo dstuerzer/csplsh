f3ad1b302ab6b76230c681cf6c5ca5bb30924f9e
Dominik Stuerzer <dominik.stuerzer@gmail.com>
2018-04-13 21:51:52 -0400

    edit guessing code: hashes < 1000, display hashes

--- hash.py
+++ hash.py
@@ -5,7 +5,13 @@ Created on Fri Apr  6 11:24:20 2018
 
 @author: Dominik
 """
+from time import time
 
+def wait():
+    t = time()
+    while ((time()-t) < 0.5):
+        1
+        
 e = 2561737     # exponent
 n = 5043197     # module
 
@@ -19,13 +25,17 @@ while True:
     print("\nThe new block is:           " + prev_hash + ' ' + transaction +' xx')
     
     while True:
-        nounce = input("Guess the nounce: ")
+        wait()
+        nounce = input("\nGuess the nounce: ")
+        wait()
         block_number = int(block+nounce)
-        hash_block = pow(block_number, e, n)    # computing the hash
-        if hash_block < n/N:
-            print("CORRECT! The completed block is " + prev_hash + ' ' + transaction +' '+nounce)
+        hash_block = pow(block_number, e, n) % 1000    # computing the hash
+        if hash_block < 1000/N:
+            print("         CORRECT!    Hash: {}\n         The completed block is ".format(hash_block) + prev_hash + ' ' + transaction +' '+nounce)
             input("")
             break
+        else:
+            print("         TRY AGAIN! Hash: {}".format(hash_block))
     
     prev_hash = str(hash_block % 1000)
     print("#####################\nMINING THE NEXT BLOCK!\n\nPrevious hash is "+prev_hash)
